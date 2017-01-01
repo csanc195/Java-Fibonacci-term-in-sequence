a545708ab48463c725af4bdf1342a78093bcfd16
Carlos <csanc195@fiu.edu>
2017-01-01 00:37:44 -0500

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,35 +1,32 @@
 
 /*************************************************************************************************** 
 	Purpose/Description: 
-			The following program includes a Final method to calculate a given term of the 
-	 	fibonacci's sequence. 
-	 		This method uses the fact that the relation of the fibonacci numbers
-	 	Fn = Fn-1 + Fn-2 can be expressed as two dimensional system of equations in matrix form. 
-		This matrix representation gives the follwoing closed expresion for the fibonacci numbers:
+	The following program includes a Final method to calculate a given term of the 
+fibonacci's sequence. 
+	This method uses the fact that the relation of the fibonacci numbers
+Fn = Fn-1 + Fn-2 can be expressed as two dimensional system of equations in matrix form. 
+This matrix representation gives the follwoing closed expresion for the fibonacci numbers:
 
-		|1 1|^n   |Fn+1 Fn	|
-		|1 0|   = |Fn   Fn-1| 
-		This identity allows the calculation of any fibonacci number by exponentiating the matrix 
-		to the needed power. 
-			This implementation alone would not be more effective, than the regular linear solution 
-		which is inherently fast(O(n)). 
-		The proposed solution for calculating a given term in the fibonacci sequence uses the exponentiation
-		of the fibonaccy matrix to its advantage by using exponentiation by squaring, an 
-		algorithm  allows the fast computation of large positive integer powers by reducing the number 
-		of multiplications needed to achieve the final solution.
-		The algorithm can be easily described by the following piecewise function:
+|1 1|^n   |Fn+1 Fn  |
+|1 0|   = |Fn   Fn-1| 
+This identity allows the calculation of any fibonacci number by exponentiating the matrix 
+to the needed power. 
+	This implementation alone would not be more effective, than the regular linear solution 
+which is inherently fast(O(n)). 
+The proposed solution for calculating a given term in the fibonacci sequence uses the exponentiation
+of the fibonaccy matrix to its advantage by using exponentiation by squaring, an 
+algorithm  allows the fast computation of large positive integer powers by reducing the number 
+of multiplications needed to achieve the final solution.
+The algorithm can be easily described by the following piecewise function:
 
-				|(x)(x^2)^((n-1)/2) if n is odd.
-		x^n =	|
-				|(x)^(2)^(n/2)  	if n is even.
-		This allows the rapid exponentiation of the fibonacci matrix, reducing and setting the number of 
-		needed multiplications logarithmically. A brief analysis shows that the time complexity 
-		of the algorithm that implements exponentiation by squaring to be O(log(n)).
-		Included in this program is also a linear implementation to be used as reference and comparison to the 
-		solution.
-	Author’s Panther ID: <5446178>
-	Certification:
-		I hereby certify that this work is my own and none of it is the work of any other person.
+	|(x)(x^2)^((n-1)/2) if n is odd.
+x^n =	|
+	|(x)^(2)^(n/2)  	if n is even.
+This allows the rapid exponentiation of the fibonacci matrix, reducing and setting the number of 
+needed multiplications logarithmically. A brief analysis shows that the time complexity 
+of the algorithm that implements exponentiation by squaring to be O(log(n)).
+Included in this program is also a linear implementation to be used as reference and comparison to the 
+solution.
  **************************************************************************************************/
 
 
